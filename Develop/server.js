7b1f9819e970f99c83c402f1ddc67cc02ca68ed5
unknown <nukasine@gmail.com>
2021-03-24 17:58:18 -0400

    added some starter code from activities in unit 11 to get my server started. Also added a routes folder.

--- Develop/server.js
+++ Develop/server.js
@@ -1,2 +1,17 @@
 const http = require('http');
 const fs = require('fs');
+
+var PORT = 8080;
+
+var server = http.createServer(handleRequest);
+
+function handleRequest(req, res) {
+  var path = req.url;
+
+
+  res.end(`It Works!! Path Hit: ${path}`);
+};
+
+server.listen(PORT, function() {
+  console.log("Server is listening at localhost:" + PORT);
+});
