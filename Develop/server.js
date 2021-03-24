44d12ea1c1c9c9c662555d576b247468388da1ec
unknown <nukasine@gmail.com>
2021-03-24 18:44:28 -0400

    Used star wars activity to work out the pathing for a basic homepage to render upon reaching the / route.

--- Develop/server.js
+++ Develop/server.js
@@ -7,11 +7,27 @@ var server = http.createServer(handleRequest);
 
 function handleRequest(req, res) {
   var path = req.url;
+  let baseFilePath = `${__dirname}/public/`;
+  let filePath;
 
+  switch (path) {
+    case '/':
+      return getHtml(baseFilePath + 'index.html', res)
+  }
 
   res.end(`It Works!! Path Hit: ${path}`);
 };
 
+
+function getHtml(filePath, res) {
+  return fs.readFile(filePath, (err, data) => {
+    if (err) throw err;
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end(data);
+  });
+}
+
+
 server.listen(PORT, function() {
-  console.log("Server is listening at localhost:" + PORT);
+  console.log("Server is listening at http://localhost:" + PORT);
 });
