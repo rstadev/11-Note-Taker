a4944443a8a6e72a325af6cd28c141057da02085
unknown <nukasine@gmail.com>
2021-03-28 15:39:28 -0400

    pivoting to a completely different server set up. Realized that my previous set up was fine for retrieving files, but not for interacting closely back and forth with the server.

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -1,6 +1,22 @@
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'view.html')));
+const path = require('path');
 
-app.get('/add', (req, res) => res.sendFile(path.join(__dirname, 'add.html')));
 
-// Displays all characters
-app.get('/api/characters', (req, res) => res.json(characters));
+
+
+
+
+
+
+
+
+
+
+
+
+
+// app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'view.html')));
+
+// app.get('/add', (req, res) => res.sendFile(path.join(__dirname, 'add.html')));
+
+// // Displays all characters
+// app.get('/api/characters', (req, res) => res.json(characters));
--- server.js
+++ server.js
@@ -1,34 +1,43 @@
-const http = require('http');
-const fs = require('fs');
-const { get } = require('https');
+// const http = require('http');
+// const fs = require('fs');
+// const { get } = require('https');
+const express = require("express");
+// const path = require("path");
 
+const app = express();
 var PORT = 8080;
 
-var server = http.createServer(handleRequest);
-
-function handleRequest(req, res) {
-  var path = req.url;
-  let baseFilePath = `${__dirname}/public/`;
-  // let filePath;
-
-  switch (path) {
-    case '/':
-      return getHtml(baseFilePath + 'index.html', res);
-    case '/notes':
-      return getHtml(baseFilePath +  'notes.html', res)
-  }
-  // `It Works!! Path Hit: ${path}`
-  res.end();
-};
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 
-function getHtml(filePath, res) {
-  return fs.readFile(filePath, (err, data) => {
-    if (err) throw err;
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end(data);
-  });
-}
+// var server = http.createServer(handleRequest);
+
+// function handleRequest(req, res) {
+//   var path = req.url;
+//   let baseFilePath = `${__dirname}/public/`;
+//   // let filePath;
+
+//   switch (path) {
+//     case '/':
+//       return getHtml(baseFilePath + 'index.html', res);
+//     case '/notes':
+//       return getHtml(baseFilePath +  'notes.html', res)
+//   }
+//   // `It Works!! Path Hit: ${path}`
+//   res.end();
+// };
+
+
+require('./routes/htmlroutes.js')(app);
+
+// function getHtml(filePath, res) {
+//   return fs.readFile(filePath, (err, data) => {
+//     if (err) throw err;
+//     res.writeHead(200, { 'Content-Type': 'text/html' });
+//     res.end(data);
+//   });
+// }
 
 
 server.listen(PORT, function() {
